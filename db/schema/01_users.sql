c6845e24eebd29e673c9e29fa8d68a3cf6fe8008
Jay <hollowic@hotmail.com>
2020-01-11 21:09:42 +0000

    template queries

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -17,10 +17,11 @@ CREATE TABLE users
 (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
-  email VARCHAR(255),
-  password VARCHAR(255),
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
   profile_pic TEXT,
-  created_at TIMESTAMP DEFAULT now()
+  created_at TIMESTAMP DEFAULT now(),
+  is_active BOOLEAN DEFAULT true
 );
 
 CREATE TABLE categories
@@ -38,7 +39,8 @@ CREATE TABLE resources
   title TEXT NOT NULL,
   description TEXT,
   url TEXT NOT NULL,
-  created_at TIMESTAMP DEFAULT now()
+  created_at TIMESTAMP DEFAULT now(),
+  is_active BOOLEAN DEFAULT true
 );
 
 CREATE TABLE comments
