f5aa94a0b7510229104fb3babc0d3c9047c24f6f
AliceMathews <alice_mathews@hotmail.co.uk>
2020-01-11 05:51:29 +0000

    Added legit data, content type is currently commented out, to be added

--- db/seeds/03_resources.sql
+++ db/seeds/03_resources.sql
@@ -5,3 +5,28 @@ VALUES
     (1, 2, 'flexbox', 'css bro', 'example.com'),
     (2, 3, 'tags', 'html bro', 'example.com'),
     (2, 4, 'queries', 'sql bro', 'example.com');
+
+-- ARTICLES
+    (1, 1, 'Javascript Style Guide', 'How to write javascript with correct style', 'https://github.com/airbnb/javascript');
+    (1, 2, 'Positioning methods in CSS', 'Overview of the different methods to position elements in css', 'https://learn.shayhowe.com/advanced-html-css/detailed-css-positioning/');
+    (1, 2, 'CSS Basics', 'Overview of CSS', 'https://learnxinyminutes.com/docs/css/');
+    (1, 2, 'Specificity', 'Explanation of specificity in CSS', 'https://www.smashingmagazine.com/2007/07/css-specificity-things-you-should-know/');
+    (1, 2, 'Responsive web design', 'Different methods to go about responsive design in web', 'https://developers.google.com/web/fundamentals/design-and-ux/responsive/patterns');
+
+-- VIDEOS
+    (1, 1, 'Recursion', 'How to implement recursion in Javascript', 'https://www.youtube.com/watch?v=k7-N8R0-KY4');
+    (1, 1, 'Promises', 'How to use promises in Javascript', 'https://www.youtube.com/watch?v=2d7s3spWAzo');
+    (1, 1, 'Closers', 'What are closures?', 'https://www.youtube.com/watch?v=CQqwU2Ixu-U');
+    (1, 2, 'Architecture', 'How to build good CSS architecture', 'https://www.youtube.com/watch?v=Nxnp-JLgo5I');
+    (1, 4, 'Database design basics', 'Basic introduction to Sequel', 'https://www.youtube.com/watch?v=LKZYoeRfmh4');
+    (1, 3, 'Tags', 'Intro to tags in HTML', 'https://www.youtube.com/watch?v=dmovVa0jseU');
+
+
+-- GAMES
+    (1, 2, 'Learn Flexbox', 'Interactive game to learn how to use flexbox', 'https://flexboxfroggy.com/');
+    (1, 2, 'Learn CSS grid', 'Interactive game to learn how to use css grid', 'https://cssgridgarden.com/');
+    (1, 1, 'Elevator saga', 'Game to practise javascript skills', 'http://play.elevatorsaga.com/');
+    (1, 1, 'Checkio Game', 'Games to practise javascript coding', 'https://checkio.org/');
+    (1, 2, 'CSS DinerGame', 'Games to learn CSS', 'https://flukeout.github.io/');
+
+
