b08d7bdfe236616f967ae5f2ff07dd9df73db913
Jay <hollowic@hotmail.com>
2020-01-11 06:46:26 +0000

    Added awesomefont CDN and icons to index.html, pkg.json

--- routes/users.js
+++ routes/users.js
@@ -5,20 +5,19 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
-module.exports = (db) => {
+module.exports = db => {
   router.get("/", (req, res) => {
+    console.log(req);
     db.query(`SELECT * FROM users;`)
       .then(data => {
         const users = data.rows;
         res.json({ users });
       })
       .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+        res.status(500).json({ error: err.message });
       });
   });
   return router;
