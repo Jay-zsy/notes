03c6203f0e2a8efc85a66993aa3b56bfa8946f44
Jay <hollowic@hotmail.com>
2020-01-12 05:34:52 +0000

    Added some comments to the comments table

--- db/seeds/04_comments.sql
+++ db/seeds/04_comments.sql
@@ -1,5 +1,23 @@
 INSERT INTO comments
     (user_id, resource_id, message)
 VALUES
-    (1, 1, 'iduno'),
-    (1, 3, 'idk');
+    (1, 1, 'I really like this post!'),
+    (1, 2, 'Does anyone know what the difference is between px and pt?'),
+    (1, 3, 'Very well presented.'),
+    (1, 4, 'Style is everything!'),
+    (1, 5, 'Great article'),
+    (1, 6, 'Now I know the difference between fixed and absolute'),
+    (1, 7, 'Well put together!'),
+    (1, 8, 'Now I can design for tablets hmm'),
+    (1, 9, 'MPJ is awesome =)'),
+    (1, 10, 'Great video!'),
+    (2, 1, 'I love callbacks!'),
+    (2, 2, 'This was super useful!'),
+    (2, 3, 'An entire course. WOW!'),
+    (2, 4, 'Style == taste imo'),
+    (2, 5, 'This article was great tbh'),
+    (2, 6, 'Good read'),
+    (2, 7, 'Anyone know what Pseudo-classes are?'),
+    (2, 8, 'Now I can design for mobile wooo!'),
+    (2, 9, 'I_HATE = RECURSION!!! => () { I_HATE() }'),
+    (2, 10, 'Promise me a job?');
