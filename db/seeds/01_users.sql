8ebc58720bcd4204be4bcdc95116a1c588dcb42f
Jay <hollowic@hotmail.com>
2020-01-13 20:53:17 +0000

    Added fake profile_pic

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -2,5 +2,5 @@
 INSERT INTO users
     (name, email, password, profile_pic)
 VALUES
-    ('Alice', 'bar@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'https://ca.slack-edge.com/T2G8TE2E5-U7986LU3B-gfe9d9417cb2-48'),
-    ('Jay', 'foo@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'https://ca.slack-edge.com/T2G8TE2E5-U7986LU3B-gfe9d9417cb2-48');
+    ('Alice', 'bar@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'https://66.media.tumblr.com/29b31b69db7b9b0a16cdc2a8e781c2bf/ca08c0ab6f9979ea-fc/s2048x3072/8ca9e112d942c6bff099c08d537e5f91620d70ac.jpg'),
+    ('Jay', 'foo@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'https://66.media.tumblr.com/avatar_d1544cd34dc5_128.pnj');
