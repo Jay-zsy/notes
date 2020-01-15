1e8ec65dc688ca8d8138d0613a6e714208842f61
Jay <hollowic@hotmail.com>
2020-01-15 01:54:02 +0000

    changed creation date

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,6 +1,6 @@
 -- Users table seeds here (Example)
 INSERT INTO users
-    (name, email, password, profile_pic)
+    (name, email, password, profile_pic, created_at)
 VALUES
-    ('Alice', 'bar@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'https://66.media.tumblr.com/avatar_97d1f91079df_128.pnj'),
-    ('Jay', 'foo@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'https://66.media.tumblr.com/avatar_d1544cd34dc5_128.pnj');
+    ('Alice', 'bar@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'https://66.media.tumblr.com/avatar_97d1f91079df_128.pnj', '2020-01-10T22:09:47.778Z'),
+    ('Jay', 'foo@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 'https://66.media.tumblr.com/avatar_d1544cd34dc5_128.pnj', '2020-01-11T22:01:32.778Z');
