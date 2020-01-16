9e8a09eb26ebfc5eddda3610e08272415417a99c
Jay <hollowic@hotmail.com>
2020-01-16 03:17:55 +0000

    finishing tiles footer

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -27,27 +27,30 @@ $(() => {
     $(".user-password-input").toggle(400);
   });
 
-  //
   $(".fa-heart").click(function() {
-    if ($(this).hasClass("far")) {
+    if ($(this).hasClass("not-liked")) {
       $.ajax({
         method: "POST",
         url: `/api/resources/${this.id}/likes`
       }).done(data => {
-        $(`#${this.id}`).text(data.number_of_likes);
         $(`#${this.id}`)
-          .removeClass("far")
-          .addClass("fas");
+          .siblings()
+          .text(data.number_of_likes);
+        $(`#${this.id}`)
+          .removeClass("not-liked")
+          .addClass("liked");
       });
-    } else if ($(this).hasClass("fas")) {
+    } else if ($(this).hasClass("liked")) {
       $.ajax({
         method: "POST",
         url: `/api/resources/${this.id}/likes/delete`
       }).done(data => {
-        $(`#${this.id}`).text(data.number_of_likes);
         $(`#${this.id}`)
-          .removeClass("fas")
-          .addClass("far");
+          .siblings()
+          .text(data.number_of_likes);
+        $(`#${this.id}`)
+          .removeClass("liked")
+          .addClass("not-liked");
       });
     }
   });
